a52ca0cf6b1afbd944768dfafc17ce00681aa871
Rachel Moore <moorer@gcsnc.com>
2020-09-30 07:57:20 -0400

    put js in separate file, created highscoreswip to use as I fix bugs

---
+++ script.js
@@ -0,0 +1,160 @@
+var timeEl = document.querySelector("#seconds");
+	var secondsLeft = 30
+
+
+	var score = 0
+	score.textContent = count;
+	var count = localStorage.getItem("count");
+	
+
+        // hides the question until start quiz is clicked
+		$(document).ready(function() {
+    	$('.question1').hide();
+        $('.question2').hide();
+        $('.question3').hide();
+        $('.question4').hide();
+		});
+
+   // start button is clicked, shows the first question and hides start button
+      $('#btn2').on('click', function() {
+			$('.question1').show();
+			$('#btn2').hide();
+			setTime();
+		});
+
+		function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+	timeEl.textContent = secondsLeft + ' Seconds Left'
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+function sendMessage() {
+  timeEl.textContent = "Times Up!";
+  localStorage.setItem("secondsLeft", secondsLeft);
+			window.location.href = "highscores.html";
+
+
+}
+
+
+        // When an answer is clicked, the question gets hidden and the next question is shown.
+		$('#btnA').on('click', function() {
+			$('.question1').hide();
+            $('.question2').show();
+            
+
+		});
+
+        $('#btnB').on('click', function(){
+			$('.question1').hide();
+            $('.question2').show();
+			secondsLeft=secondsLeft-5
+
+		});
+
+        $('#btnC').on('click', function(){
+			$('.question1').hide();
+            $('.question2').show();
+			secondsLeft=secondsLeft-5
+		});
+// show question 2
+        $('#btn2A').on('click', function() {
+			$('.question2').hide();
+            $('.question3').show();
+			secondsLeft=secondsLeft-5
+		});
+
+        $('#btn2B').on('click', function(){
+			$('.question2').hide();
+            $('.question3').show();
+			secondsLeft=secondsLeft-5
+		});
+
+        $('#btn2C').on('click', function(){
+			$('.question2').hide();
+            $('.question3').show();
+            
+		});
+// show question 3
+        $('#btn3A').on('click', function() {
+			$('.question3').hide();
+            $('.question4').show();
+			secondsLeft=secondsLeft-5
+		});
+
+        $('#btn3B').on('click', function(){
+			$('.question3').hide();
+            $('.question4').show();
+			secondsLeft=secondsLeft-5
+		});
+
+        $('#btn3C').on('click', function(){
+			$('.question3').hide();
+            $('.question4').show();
+            
+		});
+// show question 4
+        $('#btn4A').on('click', function() {
+			$('.question3').hide();
+            $('.question4').show();
+			secondsLeft=secondsLeft-5
+		});
+
+        $('#btn4B').on('click', function(){
+			$('.question3').hide();
+            $('.question4').show();
+            
+		});
+
+        $('#btn4C').on('click', function(){
+			$('.question3').hide();
+            $('.question4').show();
+			secondsLeft=secondsLeft-5
+		});
+
+// End Questions, show score
+		btn4A.addEventListener('click', function() {
+			localStorage.setItem("secondsLeft", secondsLeft);
+			window.location.href = "highscores.html";
+		})
+		
+		btn4B.addEventListener('click', function() {
+			localStorage.setItem("secondsLeft", secondsLeft);
+			window.location.href = "highscores.html";
+		})
+		
+		btn4C.addEventListener('click', function() {
+			localStorage.setItem("secondsLeft", secondsLeft);
+			window.location.href = "highscores.html";
+		})
+		
+
+        // $('#btn4C').on('click', function(){
+        // score++
+        // console.log("score", score);
+		// window.location.href = "highscores.com";
+		// });
+		
+
+// 		code fix from stack overflow
+// 		// For adding scores
+// $('#btnA, #btn2B, #btn3x, ....').click(function() { 
+//   score++; 
+// });
+
+// // for hiding and showing 
+// // do change all classes to just 'question'
+
+// // or use [class^=question] as the selector
+
+// $('.question > button').click(function() {
+//   $(this).parent().hide().next('.question').show();
+// });
+
+
